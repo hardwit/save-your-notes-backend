032143ab4560581aca4981fab1ae34460a84de7c
Ivan Minakov <minakov@reksoft.ru>
2017-06-09 15:30:04 +0300

    winston added (for logs)

---
+++ src/log.js
@@ -0,0 +1,26 @@
+const winston = require('winston');
+
+winston.emitErrs = true;
+
+function logger(module) {
+
+  return new winston.Logger({
+    transports : [
+      new winston.transports.Console({
+        level: 'debug',
+        label: getFilePath(module),
+        handleException: true,
+        json: false,
+        colorize: true,
+        prettyPrint: true
+      })
+    ],
+    exitOnError: false
+  });
+}
+
+function getFilePath (module ) {
+  return module.filename.split('/').slice(-2).join('/');
+}
+
+module.exports = logger;
